6e18b093d19312821fed048e9ad83f8113341b1d
jack <yanfeixin123@163.com>
2015-11-28 10:56:43 +0800

    wrote a ZIShuLoveLife project

---
+++ ZIShuLoveLife/app/src/main/java/com/zodiac/zishulovelife/zishulovelife/MainActivity.java
@@ -0,0 +1,17 @@
+package com.zodiac.zishulovelife.zishulovelife;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+       //@TODO:把框架搭完整， 各种需要的第三方都需要引入，， 推送用Umeng ，
+
+    }
+}
